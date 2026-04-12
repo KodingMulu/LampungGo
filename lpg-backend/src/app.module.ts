4b987994b2e957a2198a12f8efde554f2583d149
NzrlAfndi <fbotzyt@gmail.com>
2026-04-12 09:07:14 +0700

    backend: configure email for verification code

--- lpg-backend/src/app.module.ts
+++ lpg-backend/src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      },
+    }),
+    PrismaModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
