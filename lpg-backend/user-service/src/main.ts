df5519c17d043ff4e368453a1a00dee34cf0bedf
NzrlAfndi <fbotzyt@gmail.com>
2026-05-02 11:59:55 +0700

    backend: add for RBAC

--- lpg-backend/user-service/src/main.ts
+++ lpg-backend/user-service/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: '127.0.0.1',
+      port: 9003,
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(8002);
+
+  console.log('User Service (HTTP) is running on port 8002');
+  console.log('User Microservice (TCP) is listening on port 9003');
 }
 bootstrap();
