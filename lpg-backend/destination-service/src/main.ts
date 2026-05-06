7f573a01237eb0f6636db02e3f23bbc532b52dca
NzrlAfndi <fbotzyt@gmail.com>
2026-05-06 07:24:56 +0700

    backend: setup project for destination service manage from role mitra

--- lpg-backend/destination-service/src/main.ts
+++ lpg-backend/destination-service/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('api');
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: '127.0.0.1',
+      port: 9004,
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(8003);
+
+  console.log('Destination Service (HTTP) is running on port 8003');
+  console.log('Destination Microservice (TCP) is listening on port 9004');
 }
 bootstrap();
