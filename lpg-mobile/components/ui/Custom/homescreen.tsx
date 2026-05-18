834079bcf0c39fb0c3a067e5fc27a5aae18b1f42
EgyMartha <egy_martha_muhadi@teknokrat.ac.id>
2026-05-18 12:34:13 +0700

    wisata id 1

--- lpg-mobile/components/ui/Custom/homescreen.tsx
+++ lpg-mobile/components/ui/Custom/homescreen.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 import { StyleSheet, Text, View, FlatList, Image, SafeAreaView, TouchableOpacity } from 'react-native';
 
 const wisataLampung = [
-    
+    { id: '1', name: 'Pulau Pahawang', location: 'Pesawaran, Lampung', image: 'https://picsum.photos/400/200?random=1' },
 ];
