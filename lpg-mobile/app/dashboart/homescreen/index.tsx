2b23a48d9453e25d3c852a4f3f64989fa3149ae0
EgyMartha <egy_martha_muhadi@teknokrat.ac.id>
2026-05-18 13:40:25 +0700

    mobile:index homescreen fix error

--- lpg-mobile/app/dashboart/homescreen/index.tsx
+++ lpg-mobile/app/dashboart/homescreen/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
 import { StatusBar } from 'react-native';
-import HomeScreen from '../../components/ui/Custom/HomeScreen';
+import HomeScreen from '../../../components/ui/Custom/HomeScreen';
 
 export default function App() {
     <StatusBar barStyle="dark-content" />
