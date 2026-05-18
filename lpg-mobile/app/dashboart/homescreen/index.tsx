97dc3bce6cf218333f59fa458fb929525d84b88c
EgyMartha <egy_martha_muhadi@teknokrat.ac.id>
2026-05-18 13:39:47 +0700

    mobile:index homescreen

---
+++ lpg-mobile/app/dashboart/homescreen/index.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import { StatusBar } from 'react-native';
+import HomeScreen from '../../components/ui/Custom/HomeScreen';
+
+export default function App() {
+    <StatusBar barStyle="dark-content" />
+  return <HomeScreen />;
+}
