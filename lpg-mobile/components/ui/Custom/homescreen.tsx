554d12ac6b002002b4e8f8bbd5401a1b1652cd67
EgyMartha <egy_martha_muhadi@teknokrat.ac.id>
2026-05-18 12:54:12 +0700

    mobile:header title

--- lpg-mobile/components/ui/Custom/homescreen.tsx
+++ lpg-mobile/components/ui/Custom/homescreen.tsx
@@ -37,4 +37,5 @@ return (
 
 const styles = StyleSheet.create({
     container: { flex: 1, backgroundColor: '#F7F9FC' },
+    headerTitle: { fontSize: 24, fontWeight: 'bold', margin: 20, color: '#333' },
 });
