b2c010cd32431ff1c4339b4a19884bba4b70185b
EgyMartha <egy_martha_muhadi@teknokrat.ac.id>
2026-05-18 12:34:53 +0700

    wisata id 2

--- lpg-mobile/components/ui/Custom/homescreen.tsx
+++ lpg-mobile/components/ui/Custom/homescreen.tsx
@@ -4,4 +4,5 @@ import { StyleSheet, Text, View, FlatList, Image, SafeAreaView, TouchableOpacity
 
 const wisataLampung = [
     { id: '1', name: 'Pulau Pahawang', location: 'Pesawaran, Lampung', image: 'https://picsum.photos/400/200?random=1' },
+    { id: '2', name: 'Taman Nasional Way Kambas', location: 'Lampung Timur', image: 'https://picsum.photos/400/200?random=2' },
 ];
