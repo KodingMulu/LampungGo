b2e34f41961f3c0d800244ee65ff16dfeab82f09
EgyMartha <egy_martha_muhadi@teknokrat.ac.id>
2026-06-07 01:14:18 +0700

    mobile:button penginapan

--- lpg-mobile/components/ui/Custom/homescreen.tsx
+++ lpg-mobile/components/ui/Custom/homescreen.tsx
@@ -19,4 +19,5 @@ const categories = [
   { id: 1, name: 'Wisata Alam', icon: 'umbrella-beach', color: '#E8F4F8', iconColor: '#2B6CB0' },
   { id: 2, name: 'Kuliner', icon: 'soup', color: '#FEF3E2', iconColor: '#D9A74A', type: 'font-awesome' },
   { id: 3, name: 'Budaya', icon: 'theater-masks', color: '#FDF2F2', iconColor: '#C53030' },
+  { id: 4, name: 'Penginapan', icon: 'office-building', color: '#F3F4F6', iconColor: '#4B5563', type: 'material' },
 ];
